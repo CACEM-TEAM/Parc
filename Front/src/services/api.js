03660a6d740855947e2117c94d278cf615619d45
maric-urs <79602853+maric-urs@users.noreply.github.com>
2025-10-29 08:06:01 -0400

    Update api.js

--- Front/src/services/api.js
+++ Front/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // Configuration de base d'axios
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:4501/api'
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://parcauto.cacem.fr/api'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
